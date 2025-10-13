803c0d832832405d0042e3382edb7860541db5f8
ilovethebeatles <iakushkova.es@phystech.edu>
2025-10-13 22:21:08 +0300

    add interface for graphics

--- src/main/java/ru/mipt/bit/platformer/graphics/GameUnitGraphics.java
+++ src/main/java/ru/mipt/bit/platformer/graphics/GameUnitGraphics.java
@@ -3,29 +3,9 @@ package ru.mipt.bit.platformer.graphics;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 
-public abstract class GameUnitGraphics {
-    protected TextureRegion textureRegion;
-    protected Rectangle bounds;
-    protected float rotation;
-
-    public GameUnitGraphics(TextureRegion textureRegion) {
-        this.textureRegion = textureRegion;
-        this.bounds = new Rectangle();
-        this.bounds.setSize(textureRegion.getRegionWidth(), textureRegion.getRegionHeight());
-        this.rotation = 0f;
-    }
-
-    public TextureRegion getTextureRegion() {
-        return textureRegion;
-    }
-
-    public Rectangle getBounds() {
-        return new Rectangle(bounds);
-    }
-
-    public float getRotation() {
-        return rotation;
-    }
-
-    public abstract void update(float deltaTime);
+public interface GameUnitGraphics {
+    TextureRegion getTextureRegion();
+    Rectangle getBounds();
+    float getRotation();
+    void update(float deltaTime);
 }
--- src/main/java/ru/mipt/bit/platformer/graphics/impl/TankGraphics.java
+++ src/main/java/ru/mipt/bit/platformer/graphics/impl/TankGraphics.java
@@ -2,23 +2,34 @@ package ru.mipt.bit.platformer.graphics.impl;
 
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.math.Rectangle;
 import ru.mipt.bit.platformer.graphics.GameUnitGraphics;
+import ru.mipt.bit.platformer.model.GameUnit;
 import ru.mipt.bit.platformer.model.impl.Tank;
 import ru.mipt.bit.platformer.util.TileMovement;
 
 import static ru.mipt.bit.platformer.util.GdxGameUtils.moveRectangleAtTileCenter;
 
-public class TankGraphics extends GameUnitGraphics {
+public class TankGraphics implements GameUnitGraphics {
+    private final TextureRegion textureRegion;
+    private final Rectangle bounds;
+    private float rotation;
     private final TileMovement tileMovement;
     private final TiledMapTileLayer groundLayer;
     private final Tank tank;
 
     public TankGraphics(TextureRegion textureRegion, TileMovement tileMovement,
-                        TiledMapTileLayer groundLayer, Tank tank) {
-        super(textureRegion);
+                        TiledMapTileLayer groundLayer, GameUnit tank) {
+        this.textureRegion = textureRegion;
         this.tileMovement = tileMovement;
         this.groundLayer = groundLayer;
-        this.tank = tank;
+        if (!(tank instanceof Tank)) {
+            throw new IllegalArgumentException("TankGraphics requires Tank instance");
+        }
+        this.tank = (Tank) tank;
+        this.bounds = new Rectangle();
+        this.bounds.setSize(textureRegion.getRegionWidth(), textureRegion.getRegionHeight());
+        this.rotation = 0f;
         update(0f);
     }
 
@@ -32,5 +43,20 @@ public class TankGraphics extends GameUnitGraphics {
             moveRectangleAtTileCenter(groundLayer, bounds, tank.getCoordinates());
         }
         rotation = tank.getRotation();
+    }
+
+    @Override
+    public TextureRegion getTextureRegion() {
+        return textureRegion;
+    }
+
+    @Override
+    public Rectangle getBounds() {
+        return new Rectangle(bounds);
+    }
+
+    @Override
+    public float getRotation() {
+        return rotation;
     }
 }
